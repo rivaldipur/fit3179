8c39f82cfb2d95ba394acd129a054add1f742810
rivaldi <rivaldipurnama@gmail.com>
2023-09-26 21:16:18 +0800

    change file name of map

--- data vis 2/vis2.js
+++ data vis 2/vis2.js
@@ -1,4 +1,4 @@
-var vg_1 = "worlddata.json";
+var vg_1 = "militarymap.json";
 vegaEmbed("#military_map", vg_1).then(function(result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
