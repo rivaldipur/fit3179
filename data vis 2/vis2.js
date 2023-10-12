d4e39cca3afdb66ad66926c32746500a7629a354
rivaldi <rivaldipurnama@gmail.com>
2023-10-13 00:54:35 +0800

    upload other files

--- data vis 2/vis2.js
+++ data vis 2/vis2.js
@@ -1,12 +1,24 @@
 var vg_1 = "militarymap.json";
-var vg_2 = "gdp life.json"
+var vg_2 = "gdp life.json";
+var vg_3 = "life urban high.json";
+var vg_4 = "life urban low.json";
+
 vegaEmbed("#military_map", vg_1).then(function(result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 
-var vg_1 = "gdp life.json";
-vegaEmbed("#gdp_life", vg_1).then(function(result) {
+vegaEmbed("#gdp_life", vg_2).then(function(result) {
+        // Access the Vega view instance
+        //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#life_urban_high", vg_3).then(function(result) {
+        // Access the Vega view instance
+        //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#life_urban_low", vg_4).then(function(result) {
         // Access the Vega view instance
         //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
