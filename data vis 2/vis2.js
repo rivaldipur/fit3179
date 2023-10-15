89c88636590240153efd5960fc8d7aa0d64ba309
rivaldi <rivaldipurnama@gmail.com>
2023-10-16 01:51:41 +0800

    many changes

--- data vis 2/vis2.js
+++ data vis 2/vis2.js
@@ -1,9 +1,11 @@
-var vg_1 = "militarymap.json";
+var vg_1 = "map.json";
 var vg_2 = "gdp life.json";
 var vg_3 = "life urban high.json";
 var vg_4 = "life urban low.json";
+var vg_5 = "scatter.json";
+var vg_6 = "map life.json"
 
-vegaEmbed("#military_map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1).then(function(result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
@@ -22,3 +24,15 @@ vegaEmbed("#life_urban_low", vg_4).then(function(result) {
         // Access the Vega view instance
         //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+vegaEmbed("#scatter", vg_5).then(function(result) {
+        // Access the Vega view instance
+        //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#map_life", vg_6).then(function(result) {
+        // Access the Vega view instance
+        //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
